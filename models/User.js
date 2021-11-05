0d542fe1d59937c5f639f851a87aa3454a5e89a7
potatocandie <maftukhatularifin13@gmail.com>
2021-11-05 11:47:41 +0700

    add user model 🦸

---
+++ models/User.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: [true, 'Email is already been used'],
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [5, 'Password must be at least 5 characters'],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['manager', 'employee'],
+    default: 'manager',
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be more than 20 characters'],
+  },
+  shift: {
+    type: String,
+    enum: ['sunday', 'monday', 'tuesday', 'wednesday', 'friday', 'saturday'],
+    default: 'monday',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
